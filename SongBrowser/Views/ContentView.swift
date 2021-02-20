ecf8b0ce54caa9611aad1bddf787ae77bf66290a
Rowan Harris-Stoertz <rharrisstoertz@lcs.on.ca>
2021-02-20 10:54:20 -0500

    got actual search working

--- SongBrowser/Views/ContentView.swift
+++ SongBrowser/Views/ContentView.swift
@@ -67,8 +67,11 @@ struct ContentView: View {
     
     func fetchSongResults() {
         
+        //sanitize the search input
+        let input = searchText.lowercased().replacingOccurrences(of: " ", with: "+")
+        
         // Set the address of the JSON endpoint
-        let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song")!
+        let url = URL(string: "https://itunes.apple.com/search?term=\(input)&entity=song")!
 
         // Configure a URLRequest instance
         // Defines what type of request will be sent to the address noted above
