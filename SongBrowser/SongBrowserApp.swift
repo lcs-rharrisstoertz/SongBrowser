5950250088ced4946927e1674d680c083a06987f
Rowan Harris-Stoertz <rharrisstoertz@lcs.on.ca>
2021-02-20 09:49:49 -0500

    Initial Commit

---
+++ SongBrowser/SongBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  SongBrowserApp.swift
+//  SongBrowser
+//
+//  Created by Harris-Stoertz, Rowan on 2021-02-20.
+//
+
+import SwiftUI
+
+@main
+struct SongBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
