99408c8325a211e6efaa85b453da1456595fe519
Rowan Harris-Stoertz <rharrisstoertz@lcs.on.ca>
2021-02-22 11:37:14 -0500

    added 4 views that we will use to compose together a song detail view

---
+++ SongBrowser/Views/SongDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  SongDetailView.swift
+//  SongBrowser
+//
+//  Created by Harris-Stoertz, Rowan on 2021-02-22.
+//
+
+import SwiftUI
+
+struct SongDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SongDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        SongDetailView()
+    }
+}
